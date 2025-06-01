b74dc4a150f576db1a134dae37edc1136688cc39
RISHYAVANDHAN <rishyavandhan.v@gmail.com>
2025-06-01 21:57:50 +0200

    Delete Assignment_2/Molecular Dynamics directory (#6)

--- Assignment_2/Molecular Dynamics/src/md_kernel.cuh
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "io.h"
-
-void launch_compute_forces(Particle* d_particles, int N, float sigma, float epsilon);
-void launch_integrate(Particle* d_particles, int N, float dt);
--- Assignment_2/Molecular Dynamics/src/vtk_writer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-#include "io.h"
-
-bool write_vtk(const std::string& filename, const std::vector<Particle>& particles, int step);
